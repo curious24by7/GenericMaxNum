e7524d2580324b9222dc8c4a659fb01788de4734
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-04 22:45:09 +0530

    [soham]Find Max Integer.

---
+++ GenericMaxNum/MaxNumCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericMaxNum
+{
+    class MaxNumCheck
+    {
+        public static int MaxIntNum(int firstValue, int secondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("Any two Numbers are equal.");
+        }
+    }
+}
---
+++ GenericMaxNum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericMaxNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Max Number is ::");
+            int Output = MaxNumCheck.MaxIntNum(10, 20, 30);
+            Console.WriteLine(Output);
+            Console.ReadKey();
+        }
+    }
+}
