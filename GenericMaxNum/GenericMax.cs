70e5d958df6ebff2208fc2d6424143ee23fbc405
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-04 23:27:49 +0530

    [soham]Refactored code on Generic Class method

---
+++ GenericMaxNum/GenericMax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericMaxNum
+{
+    class GenericMax<T> where T : IComparable
+    {
+        public T[] value;
+        public GenericMax(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[^1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void PrintmaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum value is " + max);
+            Console.WriteLine("--------------------------");
+        }
+
+    }
+}
--- GenericMaxNum/Program.cs
+++ GenericMaxNum/Program.cs
@@ -10,19 +10,14 @@ namespace GenericMaxNum
             int[] intArray = { 10, 20, 30 };
             double[] doubleArray = { 50.2, 30.9, 60.50 };
             string[] stringArray = { "50", "85", "10" };
-
-            Program.MaxNum<int>(intArray);
-            Program.MaxNum<double>(doubleArray);
-            Program.MaxNum<string>(stringArray);
-
-
+            GenericMax<int> genericInt = new GenericMax<int>(intArray);
+            genericInt.PrintmaxValue();
+            GenericMax<double> genericDouble = new GenericMax<double>(doubleArray);
+            genericDouble.PrintmaxValue();
+            GenericMax<string> genericString = new GenericMax<string>(stringArray);
+            genericString.PrintmaxValue();
             Console.ReadKey();
         }
-        public static void MaxNum<T>(T[] inputArray)
-        {
-            var m = inputArray.Max();
-            Console.WriteLine(m);
-            Console.WriteLine("------------------------");
-        }
+
     }
 }
